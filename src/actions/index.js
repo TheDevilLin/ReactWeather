4a3927306f61d0f716336ef470175d7f8dd30a48
I_Lin <I_Lin@perfectcorp.com>
2018-08-03 18:02:16 +0800

    Ajax with axois

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_KEY = 'b0a682ced7ef510e701c6d51a0b69575';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  console.log('Request:', request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
